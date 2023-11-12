4fe6f5b1f74a14d92386a1f2e064b5051eb62504
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-11-12 20:17:24 +0100

    feat: add Views

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PeopleSearch
+namespace PeopleSearch.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
